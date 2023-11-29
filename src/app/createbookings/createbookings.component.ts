c8ca1d97d8b1d148090f4db6b0a96bd2f51fe00c
sreekanya <sreekanya@gmail.com>
2023-11-29 20:56:18 +0000

    added create booking functionalities

--- src/app/createbookings/createbookings.component.ts
+++ src/app/createbookings/createbookings.component.ts
@@ -15,7 +15,10 @@ export class CreatebookingsComponent implements OnInit {
   bookingsForm:FormGroup = new FormGroup({});
   loader : boolean = false;
   errResponse: any;
-
+  totalPay:any;
+  bookingsFinalObj:any;
+  driverId:object = {driverId:""};
+  cabId:object= {cabId:""};
   @Input()
   public bookingsInfo:any;
 
@@ -23,7 +26,7 @@ export class CreatebookingsComponent implements OnInit {
   public closeModel: EventEmitter<void> = new EventEmitter<void>();
 
   constructor(private modalService: NgbModal,  private fb:FormBuilder, private bookingService:CabookingsService,
-    private router:Router) { }
+    private router:Router ) {}
 
   ngOnInit(): void {
     if(this.bookingsInfo) {
@@ -32,8 +35,9 @@ export class CreatebookingsComponent implements OnInit {
       this.initialForm();
     }
   }
-
+ 
   initialForm(bookingObj: any = null) {
+    
     if (bookingObj === null) {
       // create
       this.bookingsForm = this.fb.group({
@@ -41,6 +45,8 @@ export class CreatebookingsComponent implements OnInit {
         customerName: ["", Validators.required],
         customerEmail: ["", Validators.required],
         customerPhone: ["", Validators.required],
+        cabId: ["", Validators.required],
+        driverId: ["", Validators.required],
         numPassengers: ["", Validators.required],
         bookingFrom: ["", Validators.required],
         bookingTo: ["", Validators.required],
@@ -48,24 +54,41 @@ export class CreatebookingsComponent implements OnInit {
       });
     } else {
       // update
+      this.driverId = {"driverId":bookingObj.driverId.driverId};
+      this.cabId = {"cabId":bookingObj.cabId.cabId};
+      //console.log("driverId...this.cabId...",this.driverId,this.cabId)
+      this.totalPay = bookingObj.totalPay
+      console.log("update.....", this.fb.group);
       this.bookingsForm = this.fb.group({
         bookingId: [bookingObj.bookingId, Validators.required],
         customerName: [bookingObj.customerName, Validators.required],
         customerEmail: [bookingObj.customerEmail, Validators.required],
         customerPhone: [bookingObj.customerPhone, Validators.required],
+        cabId: [bookingObj.cabId.cabId, Validators.required],
+        driverId: [bookingObj.driverId.driverId, Validators.required],
         numPassengers: [bookingObj.numPassengers, Validators.required],
         bookingFrom: [bookingObj.bookingFrom, Validators.required],
         bookingTo: [bookingObj.bookingTo, Validators.required],
-        totalPay: [bookingObj.totalPay, Validators.required]
+
+        totalPay: [this.totalPay, Validators.required]
       });
     }
   }
 
   onSubmit() {
+    
     if(this.bookingsForm.valid) {
-      if(this.bookingsForm.get('driverId')?.value) {
+      if(this.bookingsForm.get('bookingId')?.value) {
+        this.bookingsForm.patchValue({
+          driverId: this.driverId,
+          cabId:this.cabId
+        });
         this.handleUpdate();
       } else{
+        this.bookingsForm.patchValue({
+          driverId: this.driverId,
+          cabId:this.cabId
+        });
         console.log(this.bookingsForm.value);
         this.handleCreate();
       }
@@ -76,8 +99,9 @@ export class CreatebookingsComponent implements OnInit {
   }
 
   handleCreate() {
-    console.log("inside handle create",this.bookingsInfo);
+    console.log("inside handle create",this.bookingsForm.value);
     //Add booking functionality
+    
     this.bookingService.add(this.bookingsForm.value).subscribe((response:any)=>{
       // console.log(response);
       // this.router.navigateByUrl('/users');
@@ -89,6 +113,8 @@ export class CreatebookingsComponent implements OnInit {
   }
 
   handleUpdate() {
+    console.log("inside handle update",this.bookingsForm.value);
+   // this.bookingsForm.value.driverId:{"driverId":this.bookingsForm.value.driverId};
     this.bookingService.update(this.bookingsForm.getRawValue()).subscribe((response:any)=>{
       // console.log(response);
       window.location.href ="/bookings";
@@ -98,6 +124,15 @@ export class CreatebookingsComponent implements OnInit {
       })
   }
   
+  calculate(){
+
+    this.driverId = {"driverId":this.bookingsForm.value.driverId};
+    this.cabId = {"cabId":this.bookingsForm.value.cabId};
+    this.totalPay = this.bookingsForm.value.numPassengers*10;
+    
+    console.log("Total pay", this.bookingsForm.value.totalPay);
+  }
+
   close() {
     this.closeModel.emit();
   }
